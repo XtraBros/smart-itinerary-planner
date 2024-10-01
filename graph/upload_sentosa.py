8d75452836d629ce9c62d344263e3494b1fee737
Zac Wong <zacwong999@gmail.com>
2024-10-01 11:46:13 +0800

    add script to upload data to cloud

---
+++ graph/upload_sentosa.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import ast
+from pymongo import MongoClient
+import json
+import certifi
+
+with open('../config.json' ,'r') as file:
+    config = json.load(file)
+# Configuration
+mongo_connection_string = config['MONGO_CLUSTER_URI']
+database_name = config["MONGO_DB_NAME"]
+collection_name = config["DISTANCE_MATRIX"]
+poi = config["POI_DB_NAME"]
+poi_file = "../sentosa.csv"
+csv_file_path = "./distance_matrix.csv"
+
+# Connect to MongoDB
+client = MongoClient(mongo_connection_string, tlsCAFile=certifi.where())
+db = client[database_name]
+collection = db[collection_name]
+poidb = db[poi]
+
+# Read the CSV file into a DataFrame
+distmat = pd.read_csv(csv_file_path).applymap(lambda x: x.strip() if isinstance(x, str) else x)
+
+# Convert DataFrame to a list of dictionaries
+data = distmat.to_dict(orient="records")
+
+# Insert data into MongoDB
+collection.delete_many({})
+collection.insert_many(data)
+
+poidf = pd.read_csv(poi_file,encoding='latin1').applymap(lambda x: x.strip() if isinstance(x, str) else x)
+poidf['location'] = poidf['location'].apply(lambda loc: ast.literal_eval(loc))
+poidf = poidf.to_dict(orient="records")
+
+
+poidb.delete_many({})
+poidb.insert_many(poidf)
+
+print(f"Successfully uploaded {len(data)} documents to the collection '{collection_name}' , and {len(poidf)} to '{poi} in database '{database_name}'.")
